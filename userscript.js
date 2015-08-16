c6985f0f9ea27afbc1ba5449b117124f0b56a3ab
Sladey <Stricken@users.noreply.github.com>
2015-08-16 22:25:00 +0100

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -5,6 +5,8 @@
 // @author       Sladey
 // @include      https://leakforums.net/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @downloadURL  https://raw.githubusercontent.com/Stricken/LF-Group-Management/master/userscript.js
+// @updateURL    https://raw.githubusercontent.com/Stricken/LF-Group-Management/master/userscript.js
 // ==/UserScript==
 
 var url = window.location.href;
